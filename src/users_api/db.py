1f875c01509156c9400e4c4066e2e8526a0e6496
Yurii Kosiv <41447717+yurdosii@users.noreply.github.com>
2023-05-04 10:08:35 +0300

    feat: setup Docker and docker-compose (#5)

--- src/users_api/db.py
+++ src/users_api/db.py
@@ -1,14 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from src.test_stage1.settings import (
-    DATABASE_NAME,
+    MONGODB_DATABASE_NAME,
     MONGODB_URL,
-    USER_COLLECTION_NAME,
+    MONGODB_USER_COLLECTION_NAME,
 )
 
 
 async def get_user_collection():
     client = AsyncIOMotorClient(MONGODB_URL)
-    db = client[DATABASE_NAME]
-    collection = db[USER_COLLECTION_NAME]
+    db = client[MONGODB_DATABASE_NAME]
+    collection = db[MONGODB_USER_COLLECTION_NAME]
     return collection
